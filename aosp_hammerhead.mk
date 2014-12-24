7990aa5a1fefba86c1d578bf3452417140467886
Joshua Lee <lee.joshwa@gmail.com>
2014-12-24 00:24:45 -0700

    Initial changes. I will track properly going forward

--- aosp_hammerhead.mk
+++ aosp_hammerhead.mk
@@ -18,5 +18,9 @@ $(call inherit-product, device/lge/hammerhead/full_hammerhead.mk)
 PRODUCT_NAME := aosp_hammerhead
 
 PRODUCT_PACKAGES += \
-    Launcher3
+    Launcher3 \
+    TimeService \
+    shutdownlistener \
+    busybox \
+    static_busybox
 
